ac5fd74236c2b3ea4cb6156ca30c2c58f043842c
Christoph Hansen <christoph.hansen83@gmail.com>
2019-01-18 01:40:39 +0100

    added python2 & gawk requirement checks
    added python2 & gawk requirement checks

--- geoip_convert-v2-v1.sh
+++ geoip_convert-v2-v1.sh
@@ -5,25 +5,39 @@
 # https://github.com/emphazer/GeoIP_convert-v2-v1
 
 # requirements python2.7
-# ipaddr  >= (2.2.0) 
-# pygeoip >= (0.3.2)
+# ipaddr  == (2.2.0) 
+# pygeoip == (0.3.2)
 
 # chmod +x geoip_convert-v2-v1.sh
 # ./geoip_convert-v2-v1.sh NAME
 
 # choose a name for the release
 NAME="$1"
+PIP=`which pip2.7    2>>/dev/null` ; PIP="${PIP:-pip}"
+PYT=`which python2.7 2>>/dev/null` ; PYT="${PYT:-python2}"
+AWK=`which gawk      2>>/dev/null` ; AWK="${AWK:-awk}"
+
+if ! $PYT -V 2>&1 | grep -q "Python 2"; then 
+        echo "python2 is needed"
+        exit
+fi
+
+if ! $PIP -V 2>&1 | grep -q "python 2"; then 
+        echo "python pip is needed"
+        exit
+fi
+
 DATE_TODAY=$(date +"%Y%m%d")
 
 mkdir $DATE_TODAY && cd $DATE_TODAY && (
 
-        pip install ipaddr pygeoip &>/dev/null
+        $PIP install --upgrade pygeoip==0.3.2 ipaddr==2.2.0 &>/dev/null
 
         curl -s https://raw.githubusercontent.com/emphazer/mmdb-convert/master/mmdb-convert.py > mmdb-convert.py
         curl -s https://raw.githubusercontent.com/emphazer/mmutils/master/csv2dat.py           > csv2dat.py
 
         # set the name for the release
-        sed -i -r '/csv2dat.py [0-9]{8} Build/ s@csv2dat.py [0-9]{8}@'${NAME:-Custom}' '${DATE_TODAY:-20180000}'@' csv2dat.py
+        sed -i -r '/csv2dat.py [0-9]{8} Build/ s@csv2dat.py [0-9]{8}@'${NAME:-NAME}' '${DATE_TODAY:-20180000}'@' csv2dat.py
 
         chmod +x mmdb-convert.py csv2dat.py
 
@@ -31,7 +45,7 @@ mkdir $DATE_TODAY && cd $DATE_TODAY && (
         curl -s http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz     > GeoLite2-Country.tar.gz 
         # extract the decimal ip range with country code
         tar -xzf GeoLite2-Country.tar.gz && \
-        ./mmdb-convert.py GeoLite2-Country_*/GeoLite2-Country.mmdb
+        $PYT ./mmdb-convert.py GeoLite2-Country_*/GeoLite2-Country.mmdb
 
         # here comes the magical part
         # convert the decimal ip adresses to ipv4 
@@ -40,7 +54,7 @@ mkdir $DATE_TODAY && cd $DATE_TODAY && (
         awk -F',' {'print "\"" rshift(and($1, 0xFF000000), 24) "." rshift(and($1, 0x00FF0000), 16) "." rshift(and($1, 0x0000FF00), 8) "." and($1, 0x000000FF) "\",\"" rshift(and($2, 0xFF000000), 24) "." rshift(and($2, 0x00FF0000), 16) "." rshift(and($2, 0x0000FF00), 8) "." and($2, 0x000000FF) "\",\"" $1 "\",\"" $2 "\",\"" $3 "\",\"\""'} >GeoIPC.csv
 
         # generate the GeoIP.dat file with the csv file 
-        ./csv2dat.py -w GeoIP.dat mmcountry GeoIPC.csv
+        $PYT ./csv2dat.py -w GeoIP.dat mmcountry GeoIPC.csv
 
         # test the result
         geoiplookup 8.8.8.8 -f GeoIP.dat -i
